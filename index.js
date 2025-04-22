a0819e675059861c8edb297dd36fa837b2c3ff95
Bhawana <103374144+bhawana1107@users.noreply.github.com>
2025-04-23 00:53:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+const month = document.querySelector('.month');
+const year = document.querySelector('.calendar-year');
+const days = document.querySelector('.calendar-days');
+const date = document.querySelector('.calendar-date');
+
+function getCurrentDate() {
+    const today = new Date();
+    
+    const monthNames = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    
+    month.innerHTML = monthNames[today.getMonth()];
+    year.innerHTML = today.getFullYear();
+    days.innerHTML = dayNames[today.getDay()];
+    date.innerHTML = today.getDate();
+}
+
+getCurrentDate();
+const month = document.querySelector('.month');
+const year = document.querySelector('.calendar-year');
+const days = document.querySelector('.calendar-days');
+const date = document.querySelector('.calendar-date');
+
+function getCurrentDate() {
+    const today = new Date();
+    
+    const monthNames = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    
+    month.innerHTML = monthNames[today.getMonth()];
+    year.innerHTML = today.getFullYear();
+    days.innerHTML = dayNames[today.getDay()];
+    date.innerHTML = today.getDate();
+}
+
+getCurrentDate();
